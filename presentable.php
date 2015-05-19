b2463e55c8d7baf7cf54200e907a9fab397d2d95
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-05-19 10:36:09 +0530

    Documented presentable.php

--- presentable.php
+++ presentable.php
@@ -1,9 +1,10 @@
 <?php
+// Including Parsedown.php from https://github.com/erusev/parsedown
 include 'Parsedown.php';
-//mkdir("$argv[1]/$argv[2]/html/");
-// Copying the stylesheets of github
+// Copying the stylesheets of github to the directory where we would be storing the HTML files.
 copy("github-c486157afcc5f58155a921bc675afb08733fbaa8dcf39ac2104d3.css","$argv[1]/$argv[2]/html/github-c486157afcc5f58155a921bc675afb08733fbaa8dcf39ac2104d3.css");
 copy("github2-da2e842cc3f0aaf33b727d0ef034243c12ab008fd09b24868b97.css","$argv[1]/$argv[2]/html/github2-da2e842cc3f0aaf33b727d0ef034243c12ab008fd09b24868b97.css");
+// Header for proper display of UTF-8 and CSS links
 $header = "<!DOCTYPE html>
 <html class='' lang='en'>
 <head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#'>
@@ -19,46 +20,65 @@ $header = "<!DOCTYPE html>
 <body class='logged_in  env-production windows vis-public'>
 ";
 
+// The third argument of commandline.
 $i=intval($argv[3]);
+	// Calling function findissue to store data in usable format to stopgap.txt.
 	findissue("$argv[1]/$argv[2]/$i.txt");
+	// storing data in array from stopgap.txt.
 	$input = file("stopgap.txt");
+	// Creating html file by number of issue.
 	$outfile = fopen("$argv[1]/$argv[2]/html/$i.html","w+");
+	// putting header in HTML file
 	fputs($outfile,$header);
+	// in the .txt file the sixth line is the issue number
 	$issuenumber = $input[5];
+	// trimming $issuenumber
 	$issuenumber = strip_useless($issuenumber);
 	$issuenumber = strip_quote($issuenumber);
 	foreach ($input as $value)
 	{
+		// Searching and putting the Title in HTML file
 		if (strpos($value,'"title"')!==false)
 		{
 			$title = $value;
+			// trimming
 			$title = strip_useless($title);
 			$title = strip_quote($title);
 			fputs($outfile,"<h1 align='center'><a href='$issuenumber' target='_blank'>$title</h1><hr/>");
 		}
+		// Searching for username and storing as an array $usernameoutput
 		elseif (strpos($value,'"login"')!==false)
 		{
 			$username = $value;
+			// trimming
 			$username = strip_useless($username);
 			$username = strip_quote($username);
 			$usernameoutput[]=$username;
 		}
+		// Searching for time of creation of issue/comment and storing as an array $timeoutput
 		elseif (strpos($value,'"created_at"')!==false)
 		{
 			$time = $value;
+			// trimming
 			$time = strip_useless($time);
 			$time = strip_quote($time);
 			$time = time_link($time);
 			$timeoutput[]=$time;
 		}
+		// Searching for body of issue/comment and storing as an array $bodyoutput.
 		elseif (strpos($value,'"body"')!==false)
 		{
 			$body = $value;
+			// trimming
 			$body = strip_useless($body);
 			$body = str_replace("\r\n"," \r\n",$body);
+			// Some preprocessing before parsing down.
 			$body = strip_quote_body($body);
+			// Parsing down the markups by parsedown function (derived from parsedown.php)
 			$body = parsedown($body);
+			// Doing some minor changes to correct the output of parsedown.
 			$body = post_parsedown($body);
+			// trimming
 			$body = strip_quote_body($body);
 			$bodyoutput[]=$body;
 		}
@@ -66,21 +86,31 @@ $i=intval($argv[3]);
 //fputs ($outfile,commentheader('drdhaval2785','2015-05-07T08:47:20'));
 	for ($j=0;$j<count($usernameoutput);$j++)
 	{
+		// Putting the username and time in the heading of each issue / comment in HTML file
 		fputs($outfile,commentheader($usernameoutput[$j],$timeoutput[$j]));
+		// Putting the body of the issue / comment in HTML file
 		fputs($outfile,commentbody($bodyoutput[$j]));
 	}
+// Setting back the arrays to null for use again in the loop.
 $usernameoutput=array(); $timeoutput=array(); $bodyoutput=array();
+// Putting the endings in HTML
 fputs($outfile,"</body></html>");
+// Closing the HTML file
 fclose($outfile);
+// Deleting the stopgap.txt file which was used as intermediate state file from .txt -> .html
 unlink("stopgap.txt");
+
+
 
 function strip_useless($text)
 {
 	$text = trim($text);
+	// removing unnecessary data from txt file
 	$text = str_replace(array('"url": ','"title": ','"body": ','"login": ','"html_url": ','"created_at": '),array('','','','','',''),$text);
 	$text = trim($text);
 	$text = str_replace('\r\n\r\n\r\n','\r\n',$text);
 	$text = str_replace('\r\n\r\n','\r\n',$text);
+	// parsedown.php didnt handle \r\n properly. So a patch is made
 	$text = str_replace(array('\r\n'),array('
 	~~~'),$text);
 	return $text;
@@ -96,18 +126,7 @@ function strip_quote_body($text)
 	$text = preg_replace('/^(["])/','',$text);
 	$text = preg_replace('/(["],?)$/','',$text);
 	$text = str_replace('&quot;','',$text);
-//	$text = str_replace(array('"',',','&quot;'),array('','',''),$text);
 	return $text;
-}
-function make_link($text)
-{
-		$text = preg_replace('/https?:\/\/[^\s"<>]+/', '<a href="$0" target="_blank">$0</a>', $text);
-		return $text;
-}
-function user_link($text)
-{
-		$text = "https://github.com/$text";
-		return $text;
 }
 function time_link($text)
 {
@@ -116,10 +135,15 @@ function time_link($text)
 }
 function parsedown($text)
 {
-	$text = preg_replace('/\r\n```\r\n([^`]*)\r\n```\r\n/', '\r\n```$1```\r\n', $text); // Treating the blob
+	// sometimes users place line breaks after triple quotes. To overcome that issue this patch has been created.
+	$text = preg_replace('/\r\n```\r\n([^`]*)\r\n```\r\n/', '\r\n```$1```\r\n', $text);
+	// sometimes users place a space between ] and ( for linking in markup. To overcome that.
 	$text = str_replace('] (','](',$text); // Links error correction
+	// To overcome aberrent behaviour in case the parser places a \ before " of href
 	$text = str_replace('href=\"','href="',$text);
+	// Same as above
 	$text = str_replace('\">','">',$text);
+	// Calling parsedown class and parsing.
 	$Parsedown = new Parsedown();
 	return $Parsedown->text($text);
 }
@@ -136,36 +160,44 @@ function post_parsedown($input)
 		$text = preg_replace('/\*([^~]*)\*/', '<i>$1</i>', $text); // italics
 		$text = preg_replace('/^\*([^\*]*)$/', '<li>$1', $text); // unordered list
 		$text = preg_replace('/^[0-9][.]([^\*]*)/', '<li>$1', $text); // unordered list 
-		$text = str_replace('<br />','<br/>',$text);
+		$text = str_replace('<br />','<br/>',$text); // removing space after br.
 		$text = preg_replace('/@([^\W_ ]*)([\W_ ])/', '<strong><a href="https://github.com/$1" target="_blank">@$1</a></strong>$2', $text); // mentioning		
-		$text = str_replace('<pre><code></code></pre>','',$text);
-		$text = str_replace('<pre><code>~~~','<p>',$text);
-		$text = str_replace('</code></pre>','</p>',$text);
-		$text = preg_replace('/(~~~&gt;)([^~]*)(~~~)/','<blockquote>$2</blockquote>',$text);
-		$text = str_replace('<pre><code class="language-and">','and',$text);
-		$text = str_replace('&lt;','<',$text);
-		$text = str_replace('&gt;','>',$text);
-		$text = str_replace('~~~','<br/>',$text);
-		$text = str_replace('<p><p>','<p>',$text);
-		$text = str_replace('</p></p>','</p>',$text);
+		$text = str_replace('<pre><code></code></pre>','',$text); // removing superfluous codeblocks
+		$text = str_replace('<pre><code>~~~','<p>',$text); // removing superfluous codeblocks
+		$text = str_replace('</code></pre>','</p>',$text); // removing superfluous codeblocks
+		$text = preg_replace('/(~~~&gt;)([^~]*)(~~~)/','<blockquote>$2</blockquote>',$text);// blockquotes
+		$text = str_replace('<pre><code class="language-and">','and',$text); // Sometimes the codeblocks having 'and' is not properly treated, because parsedown takes it as a language.
+		$text = str_replace('&lt;','<',$text); // <
+		$text = str_replace('&gt;','>',$text); // >
+		$text = str_replace('~~~','<br/>',$text); // for browser display \r\n is converted to <br/>
+		$text = str_replace('<p><p>','<p>',$text); // removed superfluous 
+		$text = str_replace('</p></p>','</p>',$text); // removed superfluous
 		$splitout[$i] = $text;
 	}
-	$output = implode('<br/>',$splitout);
+	$output = implode('<br/>',$splitout); // Joining via <br/> for browser display
 	return $output;
 }
+
 function findissue($filepath)
 {
+	// read the file e.g. 1.txt
 	$data = file_get_contents($filepath);
+	// separate the issue and comments.
 	$issue_comment_separator = explode('BODY STARTS FROM HERE',$data);
 	$issue = $issue_comment_separator[0];
+	$comment = $issue_comment_separator[1];
+	// The regex is to prevent split on basis of [ in case of captured images or github flavored URL linking.
 	$split_issue = preg_split('/[^!][\[][^ ]/',$issue);
+	// Exploding to get appropriate piece of information from .txt to put into stopgap.txt
 	$closed_at=explode('"comments":',$split_issue[1]);
 	$closed_by=explode('"closed_by":',$closed_at[1]);
-	$comment = $issue_comment_separator[1];
+	// storing the data in stopgap.txt
 	file_put_contents('stopgap.txt',$split_issue[0].'['.$closed_by[0].$comment);
 }
+
 function commentheader($username,$time)
 {
+	// Copy pasted the github div class etc for heading of issue / comment
 	$output = '<div class="comment previewable-edit timeline-comment js-comment js-task-list-container owner-comment current-user" data-body-version="3d69eb2502aec4738da37c0867d635da">
   <div class="timeline-comment-header ">
   <div class="timeline-comment-header-text">
@@ -183,6 +215,7 @@ return $output;
 
 function commentbody($text)
 {
+	// Copy pasted the github div class etc for body of issue / comment
 	$output = '<div class="edit-comment-hide">
       <div class="comment-body markdown-body markdown-format js-comment-body">
           '.$text.'
