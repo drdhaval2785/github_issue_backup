d564eb1ee70ba41dcca016e896d958ee42c03caa
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-05-29 10:43:14 +0530

    Version 1.0.2
    logfile added

--- github_issue_backup.sh
+++ github_issue_backup.sh
@@ -9,23 +9,33 @@
 timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
-echo started execution at 
-timestamp
-echo creating directory $1/$2
 mkdir -p $1/$2/html/images || exit 1
+echo Created directory $1/$2.
+echo Created directory $1/$2. > $1/$2/log.txt
+timestamp >> $1/$2/log.txt
+echo Started fetching data from $1/$2 repository,
+timestamp
+echo Started fetching data from $1/$2 repository. >> $1/$2/log.txt 
+timestamp >> $1/$2/log.txt
 if [ "$5" != "-l" ]
 then
 # copying the necessary code for syntax highlighting. See http://alexgorbatchev.com/SyntaxHighlighter/download/ for the downloaded folder.
 cp -r syntaxhighlighter $1/$2/html
+echo Copied syntaxhighlighter folder to $1/$2 folder. >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
 # copying the necessary code for Emojis. See https://github.com/drdhaval2785/github_issue_backup/issues/11 and https://github.com/chobie/Emoji for details.
 cp -r Emoji $1/$2/html
+echo Copied Emoji folder to $1/$2 folder. >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
 fi
 a=`expr 1`
 # making iteration till the third argument (issue number till which the user wants to fetch the issues).
 while [ $a -lt `expr $3 + 1` ]
 do
-	echo started issue number $a
-	echo printing issue $a to $1/$2/$a.txt
+	echo Started issue number $a.
+	echo Started issue number $a. >> $1/$2/log.txt
+	timestamp >> $1/$2/log.txt
+	echo Printing issue $a to $1/$2/$a.txt.
    # $1 is the username, $2 is the repositoryname, $a is the issue number.
    # An explanation of the arguments passed in this curl line is in order.
    # state=all fetches all the issues (Available options are open/closed/all).
@@ -35,7 +45,7 @@ do
    # >$1/$2/$a.txt writes the data fetched by curl to the file which is numbered as per issue number e.g. drdhaval2785/SanskritVerb/1.txt
    # This completes the noting of issue in our file.
 	curl 'https://api.github.com/repos/'$1/$2'/issues/'$a'?state=all&page=1&per_page=1000&client_id=1dd1dddcb68d6148c249&client_secret=7577e3bd5cb5ad20bea86430a8ed5a29df5fa455' > $1/$2/$a.txt
-	echo Appending comments on issue $a to $1/$2/$a.txt
+	echo Appending comments on issue $a to $1/$2/$a.txt.
 	# This is the separator by which we will separate the issue and comments in presentable.php.
 	echo BODY STARTS FROM HERE >> $1/$2/$a.txt 
    # >>$1/$2/$a.txt appends the data fetched by curl to the file which is numbered as per issue number e.g. drdhaval2785/SanskritVerb/1.txt
@@ -43,13 +53,17 @@ do
 	curl 'https://api.github.com/repos/'$1/$2'/issues/'$a'/comments?state=all&page=1&per_page=1000&client_id=1dd1dddcb68d6148c249&client_secret=7577e3bd5cb5ad20bea86430a8ed5a29df5fa455' >> $1/$2/$a.txt
 	# At the end of this activity, the data in 1.txt would be of the format issue+comments thereon.
 	# incrementing for the next iteration.
-	echo preparing $a.html for display
+	echo preparing $a.html for display.
 	php presentable.php $1 $2 $a $5 # Trying to suppress the emojis / syntaxhighlighter optionally to decrease the size of folder. Default is -f = FULL. Otherwise he may enter -l as fourth argument.
-	echo completed issue number $a
+	echo Completed issue number $a.
+	echo Completed issue number $a. >> $1/$2/log.txt
+	timestamp >> $1/$2/log.txt
 	a=`expr $a + 1`
 done
-echo culled links of images
+echo Fetched links of images.
 php image_links.php $1 $2 $3
+echo Fetched links of images. >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
 # Fetching the images and storing in $1/$2/html/images/ folder.
 echo Fetching the images and storing in $1/$2/html/images/ folder.
 i=0
@@ -58,14 +72,23 @@ do
 	curl $line > $1/$2/html/images/$i.png
 	i=$(($i + 1))
 done < "imagelinks.txt"
+echo Fetched images. >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
 # substituting the image links with local links
-echo Substituting the image links with local links.
 php substitute_images.php $1 $2 $3
+echo Substituted the image links with local links.
+echo Substituted the image links with local links. >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
+echo Completed execution >> $1/$2/log.txt
+timestamp >> $1/$2/log.txt
 if [ "$4" != "-p" ]
 then
 rm -r $4/$1/$2
 mkdir -p $4/$1 || exit 1
 mv -f $1/$2 $4/$1
+rm -r $1
+echo Moved directory $1/$2 to $4/$1/$2. >> $4/$1/$2/log.txt
+timestamp >> $4/$1/$2/log.txt
 fi
 echo completed execution at 
 timestamp
