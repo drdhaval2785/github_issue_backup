7fc4cc636a4819effab295ce405fd6d7ca3828cf
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-06-20 10:37:30 +0530

    Issue 22 closed
    https://github.com/drdhaval2785/github_issue_backup/issues/22

--- cronjob_sanskrit_lexicon.bat
+++
@@ -1,5 +0,0 @@
-cd E:/C_drive/xampp/htdocs/github_issue_backup
-github_issue_backup.sh sanskrit-lexicon -a -a e:/backuptrial -l -y
-github_issue_backup.sh drdhaval2785 -a -a e:/backuptrial -l -y
-github_issue_backup.sh funderburkjim -a -a e:/backuptrial -l -y
-github_issue_backup.sh gasyoun -a -a e:/backuptrial -l -y
--- cronjob_sanskrit_lexicon.sh
+++ cronjob_sanskrit_lexicon.sh
@@ -1,4 +1,6 @@
-github_issue_backup.sh sanskrit-lexicon -a -a e:/backuptrial -l -y
-github_issue_backup.sh drdhaval2785 -a -a e:/backuptrial -l -y
-github_issue_backup.sh funderburkjim -a -a e:/backuptrial -l -y
-github_issue_backup.sh gasyoun -a -a e:/backuptrial -l -y
+cd E:/C_drive/xampp/htdocs/github_issue_backup
+sh github_issue_backup.sh sanskrit-lexicon -a -a e:/backuptrial -l -y
+sh github_issue_backup.sh drdhaval2785 -a -a e:/backuptrial -l -y
+sh github_issue_backup.sh funderburkjim -a -a e:/backuptrial -l -y
+sh github_issue_backup.sh gasyoun -a -a e:/backuptrial -l -y
+sleep 120
