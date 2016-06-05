ee2f743be9908b74a347b1a6b18592f49adc0c58
drdhaval2785 <drdhaval2785@gmail.com>
2016-06-05 11:28:01 +0530

    get_issue_number.php commented

--- get_issue_number.php
+++ get_issue_number.php
@@ -11,6 +11,7 @@
 // Hide error reportings.
 error_reporting(0);
 
+// If the user has entered `-a` i.e. all issues to download.
 if ($argv[1]==="-a")
 {
 	// Adding a new line in timelog.txt
@@ -22,10 +23,13 @@ if ($argv[1]==="-a")
 	}
 	$user = $argv[2];
 	$repo = $argv[3];
+	// Read issue.txt
 	$a=file_get_contents("issue.txt");
 	$b=json_decode($a,true);
 	$cr = '';
+	// Setting initial time to 1980
 	$times[] = '1980-01-01T00:00:01Z';
+	// Scraping updated_at times and $cr (issues to be downloaded in decreasing order, separated by a space)
 	if (count($b)>0)
 	{
 		$upper = $b[0]['number'];
@@ -36,29 +40,37 @@ if ($argv[1]==="-a")
 		}
 	}
 	$cr = trim($cr);
+	// If there is non empty $cr, display it.
 	if ($cr!=="")
 	{
 		echo $cr;
 	}
+	// If there is no issue at all in the given repository, return EXIT1, which will display proper error message to the script user.
 	elseif (count($b)===0)
 	{
 		echo 'EXIT1';
 	}
+	// If there is no issue to be updated, return EXIT, which will exit the script with proper message, that there is nothing to update.
 	else
 	{
 		echo 'EXIT';
 	}
 	// Writing a timelog.
+	// Fourth item `validated` would be written only after the whole shell script has run satisfactorily.
+	// This will ensure that user terminated / exit script timings are not added to validated database.
 	$tlog = fopen('timelog.txt','a+','utf-8');
 	fputs($tlog,$user.",".$repo.",".max($times).",");
 	fclose($tlog);
 }
-elseif (strpos($argv[1],',')!==false)
+// If the user has entered in 1:10 or 1,44,23 or 1:10,12,14 etc specifications for downloading only specific issues / range of issues
+elseif (strpos($argv[1],',')!==false || strpos($argv[1],':')!==false)
 {
+	// Segregate on basis of commas e.g. 1:10,12,14 -> array('1:10','12','14')
 	$br = explode(",",$argv[1]);
 	$cr = '';
 	foreach ($br as $val)
 	{
+		// If there is a colon, create a range e.g. 1:10 -> 10 9 8 7 6 5 4 3 2 1
 		if (strpos($val,":")!==false)
 		{
 			$lower0 = explode(":",$val); $lower=$lower0[0];
@@ -68,17 +80,20 @@ elseif (strpos($argv[1],',')!==false)
 				$cr = $cr." ".$i;
 			}
 		}
+		// Else, add it to the end
 		else
 		{
 			$cr = $cr." ".$val;
 		}
 	}
+	// Trim and display
 	$cr = trim($cr);
 	echo $cr;
 }
+// If there is no comma or colon, it means that the user has entered single issue.
 else
 {
+	// Display single issue.
 	echo $argv[1];
 }
-
 ?>
